3922acb5675cd72a33fdbe206968547bca212c93
Arturo <Arturo@b1d50d5f-df8f-9249-af92-6a0d583c2fd7>
2022-03-14 14:03:41 +0000

    Etiquetado 2.6.2 (sp4)
    git-svn-id: http://10.34.58.18/svn/svn_repository/Ulises5000I/branches/agl-dev/ulises-ta/Sources@10454 b1d50d5f-df8f-9249-af92-6a0d583c2fd7

--- TFT/HMI/Source/VersionInfo.cs
+++ TFT/HMI/Source/VersionInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.2.002")]
-[assembly: AssemblyFileVersion("2.6.2.002")]
+[assembly: AssemblyVersion("2.6.2.004")]
+[assembly: AssemblyFileVersion("2.6.2.004")]
